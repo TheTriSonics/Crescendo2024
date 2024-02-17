13488b655d4efdf2cb0fb41faa2f2355ec2a9205
Justin Buist <justin.buist@gmail.com>
2024-02-17 15:58:05 -0500

    RobotMap -> constants

---
+++ constants.py
@@ -0,0 +1,40 @@
+
+class RobotMap():
+
+    driver_controller = 0
+    commander_controller = 1
+
+    # CAN assignments
+    front_right_drive = 11
+    front_right_turn = 21
+    front_right_turn_encoder = 31
+
+    front_left_drive = 12
+    front_left_turn = 22
+    front_left_turn_encoder = 32
+
+    back_right_drive = 13
+    back_right_turn = 23
+    back_right_turn_encoder = 33
+
+    back_left_drive = 14
+    back_left_turn = 24
+    back_left_turn_encoder = 34
+
+    intake_feed = 51
+    intake_tilt = 52
+
+    amp_lift = 61
+    amp_feed = 62
+
+    shooter_left = 71
+    shooter_right = 72
+    shooter_feed = 81
+    shooter_rotate_left = 91
+    shooter_rotate_right = 92
+
+    climber_left = 9990
+    climber_right = 9989
+
+    pigeon2_id = 41
+
